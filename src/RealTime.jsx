0cce6e6ea1dcc2941affe6f74e49d222e36100ac
Abhishek Kumar <67472833+AbhishekKumar2233@users.noreply.github.com>
2022-01-18 20:16:42 +0530

    time dynamic refresh

--- src/RealTime.jsx
+++ src/RealTime.jsx
@@ -1,11 +1,16 @@
 import React, { useState } from "react";
 
 export default function RealTime() {
-  const [time, setTime] = useState("Time");
+  //setinterval function used to show data every second
+  setInterval(timefunc, 1000);
+
   var clock = new Date().toLocaleTimeString();
 
+  const [time, setTime] = useState(clock);
+
   function timefunc() {
-    setTime(clock);
+    var newclock = new Date().toLocaleTimeString();
+    setTime(newclock);
   }
   return (
     <div className="container">
