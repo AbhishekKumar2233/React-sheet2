8ef8aad2129b7e90c330427b0bf8f76aef85cea3
Abhishek Kumar <67472833+AbhishekKumar2233@users.noreply.github.com>
2022-01-18 19:10:32 +0530

    increase and decrease value

--- src/Hook1.js
+++ src/Hook1.js
@@ -4,6 +4,7 @@ import "./styles.css";
 
 function Hook1() {
   const [count, setCount] = useState(0);
+
   //const[initialvalue,function]
   //o value is stored in count
   //setCount is a function
@@ -14,16 +15,18 @@ function Hook1() {
   // const [red,green,blue,blur] = [9,132,227,1];
   // console.log(red,);
   // console.log(red,green);
-
   function increase() {
     setCount(count + 1);
   }
-
+  function decrease() {
+    setCount(count - 1);
+  }
   //using return not render
   return (
     <div className="container">
       <h1>{count}</h1>
       <button onClick={increase}>+</button>
+      <button onClick={decrease}>-</button>
     </div>
   );
 }
