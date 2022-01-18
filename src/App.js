a59376dda0aaa335a535d9b31080fe83a20d5fd5
Abhishek Kumar <67472833+AbhishekKumar2233@users.noreply.github.com>
2022-01-18 16:33:02 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./styles.css";
+import Strike from "./Strike";
+
+export default function App() {
+  var isDone = true;
+  const strikeThrough = { textDecoration: "line-through" };
+
+  return (
+    <div className="App">
+      {/* ui=f(state)
+       */}
+      <h1 style={isDone ? strikeThrough : null}>State </h1>
+      <h1 style={isDone && strikeThrough}>State </h1>
+      <Strike />
+    </div>
+  );
+}
---
+++ src/Strike.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function strikebtn() {
+  document.getElementById("root").style.backgroundColor = "red";
+}
+function strikebtn1() {
+  document.getElementById("root").style.backgroundColor = "#fff";
+}
+export default function Strike() {
+  return (
+    <div>
+      <button onClick={strikebtn}> Change Background color</button>
+      <button onClick={strikebtn1}> Remove Background color</button>
+    </div>
+  );
+}
