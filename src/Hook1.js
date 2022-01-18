488457e5e191fb8458167d11c612f9967ccf9798
Abhishek Kumar <67472833+AbhishekKumar2233@users.noreply.github.com>
2022-01-18 17:28:39 +0530

    value displayed on screen using state and it is initial value

--- src/Hook1.js
+++ src/Hook1.js
@@ -1,20 +1,21 @@
-// import React from "react";
-// import ReactDOM from "react-dom";
+import { StrictMode } from "react";
+import React, { useState } from "react";
 
-// export default function Hook1() {
-//   var count = 0;
+function Hook1() {
+  const state = useState(0);
 
-//   //changing ui using hooks
-//   //hooks used to changes content in ui
-//   function increase() {
-//     count++;
-//     console.log(count);
-//     ReactDOM.render(
-//       <div className="container">
-//         <h1>{count}</h1>
-//         <button onClick={increase}>+</button>
-//       </div>,
-//       document.getElementById("root2")
-//     );
-//   }
-// }
+  function increase() {
+    //count++;
+  }
+
+  //using return not render
+  return (
+    <StrictMode>
+      <div className="container">
+        <h1>{state[0]}</h1>
+        <button onClick={increase}>+</button>
+      </div>
+    </StrictMode>
+  );
+}
+export default Hook1;
--- src/index.js
+++ src/index.js
@@ -2,27 +2,12 @@ import { StrictMode } from "react";
 import ReactDOM from "react-dom";
 
 import App from "./App";
-
-var count = 0;
-function increase() {
-  count++;
-  console.log(count);
-  ReactDOM.render(
-    <div className="container">
-      <h1>{count}</h1>
-      <button onClick={increase}>+</button>
-    </div>,
-    document.getElementById("root")
-  );
-}
+import Hook1 from "./Hook1";
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <StrictMode>
-    <div className="container">
-      <h1>{count}</h1>
-      <button onClick={increase}>+</button>
-    </div>
+    <Hook1 />
     <App />
   </StrictMode>,
   rootElement
